ccc5795cb7b066ae8b1f386784087624be567602
0x4D2 <2046360988@qq.com>
2023-02-11 23:06:45 +0800

    git commit

--- src/main/java/com/rdfd/rdfd/RDFD.java
+++ src/main/java/com/rdfd/rdfd/RDFD.java
@@ -1,4 +1,24 @@
 package com.rdfd.rdfd;
 
-public class RDFD {
+import com.rdfd.rdfd.item.ModFoodComponents;
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.impl.registry.sync.FabricRegistry;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+import org.apache.commons.logging.Log;
+import org.slf4j.LoggerFactory;
+import org.slf4j.Logger;
+
+public class RDFD implements  ModInitializer{
+    public static final String MOD_ID = "RDFD";
+    public static final Logger LOGGER = LoggerFactory.getLogger("RDFD");
+
+    public static final Item YINDOUBAI = new Item(new Item.Settings()
+            .group(ItemGroup.FOOD).food(ModFoodComponents.YINDOUBAI));
+    @Override
+    public void onInitialize() {
+        Registry.register(Registry.ITEM, new Identifier("rdfd", "yindoubai"), YINDOUBAI);
+    }
 }
---
+++ src/main/java/com/rdfd/rdfd/item/ModFoodComponents.java
@@ -0,0 +1,8 @@
+package com.rdfd.rdfd.item;
+
+import net.minecraft.item.FoodComponent;
+
+public class ModFoodComponents {
+    public static final FoodComponent YINDOUBAI = new FoodComponent.Builder().hunger(20).
+            saturationModifier(10.0f).build();
+}

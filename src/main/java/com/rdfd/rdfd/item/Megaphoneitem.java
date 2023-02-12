dd07745d06baa0a74e5e77723721a1913bcca7c8
0x4D2 <2046360988@qq.com>
2023-02-12 10:31:08 +0800

    git commit

---
+++ src/main/java/com/rdfd/rdfd/item/Megaphoneitem.java
@@ -0,0 +1,10 @@
+package com.rdfd.rdfd.item;
+
+import net.minecraft.item.Item;
+
+public class Megaphoneitem extends Item {
+
+    public Megaphoneitem(Settings settings) {
+        super(settings);
+    }
+}
--- src/main/java/com/rdfd/rdfd/item/ModFoodComponents.java
+++ src/main/java/com/rdfd/rdfd/item/ModFoodComponents.java
@@ -1,8 +1,10 @@
 package com.rdfd.rdfd.item;
 
+import net.minecraft.entity.effect.StatusEffectInstance;
+import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.item.FoodComponent;
 
 public class ModFoodComponents {
-    public static final FoodComponent YINDOUBAI = new FoodComponent.Builder().hunger(20).
-            saturationModifier(10.0f).build();
+    public static final FoodComponent YINDOUBAI = new FoodComponent.Builder().hunger(20).saturationModifier(10.0f).statusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST,114514,100),1.0f).meat().alwaysEdible().build();
+
 }

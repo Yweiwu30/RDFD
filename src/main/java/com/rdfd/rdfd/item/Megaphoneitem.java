54d3d9f9ec136ac9a13ee109222edcda87a34c28
0x4D2 <2046360988@qq.com>
2023-02-12 10:36:42 +0800

    git commit

--- src/main/java/com/rdfd/rdfd/item/Megaphoneitem.java
+++ src/main/java/com/rdfd/rdfd/item/Megaphoneitem.java
@@ -1,10 +1,27 @@
 package com.rdfd.rdfd.item;
 
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
 
 public class Megaphoneitem extends Item {
 
     public Megaphoneitem(Settings settings) {
         super(settings);
+    }
+
+    @Override
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        say(user);
+        user.getItemCooldownManager().set(this, 20);
+        return super.use(world, user, hand);
+    }
+
+    private void say(PlayerEntity uesr) {
+        uesr.sendMessage(Text.literal("ECHO"));
     }
 }
--- src/main/java/com/rdfd/rdfd/item/ModFoodComponents.java
+++ src/main/java/com/rdfd/rdfd/item/ModFoodComponents.java
@@ -2,9 +2,16 @@ package com.rdfd.rdfd.item;
 
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.FoodComponent;
+import net.minecraft.item.ItemStack;
+import net.minecraft.text.Text;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.world.World;
+
+import java.lang.reflect.Type;
 
 public class ModFoodComponents {
     public static final FoodComponent YINDOUBAI = new FoodComponent.Builder().hunger(20).saturationModifier(10.0f).statusEffect(new StatusEffectInstance(StatusEffects.HEALTH_BOOST,114514,100),1.0f).meat().alwaysEdible().build();
-
 }
